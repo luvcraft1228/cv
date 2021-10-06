e00096a8f4eb3f0d0b138feae1efd7e57ddc1ca9
David Brais <2095059@cshawi.ca>
2021-10-06 09:52:57 -0400

    responsive ok for now

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-const rButton = document.querySelector("#right")
-const lButton = document.querySelector("#left")
-const pages = document.querySelectorAll("page")
+const rButton = document.querySelector("#right");
+const lButton = document.querySelector("#left");
+const pages = document.querySelectorAll("page");
 
 let index = 0;
+window.addEventListener("orientationchange", (event) => {
+  let angle = event.target.screen.orientation.angle;
 
-rButton.addEventListener("click",()=>{
+  if (angle == 270 || angle == 90) {
+    index = 0;
+    lButton.hidden = true;
+    rButton.hidden = false;
+  }
+  if (angle == 0 || angle == 180) {
+    lButton.hidden = true;
+    rButton.hidden = true;
+    pages.forEach((page) => {
+      page.hidden = false;
+    });
+  }
+});
 
-        if(index>=0) {
-            pages[index++].hidden=true;
-            pages[index].hidden=false;
-            lButton.hidden=false;
-        }
-        if(index==pages.length-1){
-            rButton.hidden=true;
+rButton.addEventListener("click", () => {
+  if (index >= 0) {
+    pages[index++].hidden = true;
+    pages[index].hidden = false;
+    lButton.hidden = false;
+  }
+  if (index == pages.length - 1) {
+    rButton.hidden = true;
+  }
+});
+lButton.addEventListener("click", () => {
+  if (index < pages.length) {
+    pages[index--].hidden = true;
+    pages[index].hidden = false;
+    rButton.hidden = false;
+  }
 
-        }
-
-})
-lButton.addEventListener("click",()=>{
-
-    if(index<pages.length){
-        pages[index--].hidden=true;
-        pages[index].hidden=false;
-        rButton.hidden =false;
-    }
-
-
-    if(index==0){
-        lButton.hidden=true;
-
-    }
-
-})
+  if (index == 0) {
+    lButton.hidden = true;
+  }
+});
